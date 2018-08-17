e6b1a3b3a3a166297c61d4c9f37bee6a505e06ab
Sammy Nave <sammy.nave@gmail.com>
2018-08-17 15:26:32 -0400

    dummy button

--- src/ui/components/UiButton/component.ts
+++ src/ui/components/UiButton/component.ts
@@ -1,5 +1,14 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
 
 export default class UiButton extends Component {
+  @tracked public clickCount = 0;
 
+  @tracked('clickCount')
+  get hasClicks() {
+    return this.clickCount > 0;
+  }
+
+  public click() {
+    this.clickCount = this.clickCount + 1;
+  }
 }
